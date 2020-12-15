7bbd454607ca4b68a5adf8588631b1bac7c56267
Florian Soller <flsoller@gmail.com>
2020-12-15 16:44:25 +0100

    Import and mount router. Add comments.

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
+// Package imports
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDatabase from './config/database.js';
 
+// Route imports
+import maintenanceData from './routes/maintenanceData.js';
+
 // Load env configuration
 dotenv.config();
 
-// Set database connection
+// Establish database connection
 connectDatabase();
 
+// Run express
 const app = express();
 
+// Mount routers
+app.use('/api/v1/maintenance-data', maintenanceData);
+
+// Define server port
 const PORT = process.env.PORT || 5000;
 
 app.listen(
