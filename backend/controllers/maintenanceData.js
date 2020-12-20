c35f34d933f622af313cdd77f409d14f9ff019f6
Florian Soller <flsoller@gmail.com>
2020-12-20 17:52:43 +0100

    Add controller method for creating db entries.

--- backend/controllers/maintenanceData.js
+++ backend/controllers/maintenanceData.js
@@ -1,11 +1,27 @@
+import SampleModel from '../models/SampleModel.js';
+
 // @desc    Get maintenance data
 // @route   GET /api/v1/maintenance-data
 // @access  Private
-const getMaintenanceData = async (req, res, next) => {
+export const getMaintenanceData = async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: 'Maintenance Data Object here',
   });
 };
 
-export { getMaintenanceData };
+// @desc    Create new entry for maintenance data document
+// @route   POST /api/v1/maintenance-data
+// @access  Private
+export const addMaintenanceData = async (req, res, next) => {
+  const maintenanceData = await SampleModel.create(req.body);
+
+  try {
+    res.status(200).json({
+      success: true,
+      data: maintenanceData,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
