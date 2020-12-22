ae6f3c4c6009e93ee66503cb2131f790db47ca34
Florian Soller <flsoller@gmail.com>
2020-12-22 16:46:10 +0100

    Simplify model structure for testing.

--- backend/models/SampleModel.js
+++ backend/models/SampleModel.js
@@ -1,13 +1,12 @@
 import mongoose from 'mongoose';
 
 const SampleModelSchema = new mongoose.Schema({
-  customerData: {
-    customerName: String,
-    installLocation: String,
-    street: String,
-    city: String,
-    country: String,
-  },
+  customerName: String,
+  installLocation: String,
+  street: String,
+  city: String,
+  country: String,
+
   productName: {
     type: String,
     required: [true, 'Please provide a product name'],
@@ -19,32 +18,6 @@ const SampleModelSchema = new mongoose.Schema({
   maintenanceDate: {
     type: Date,
     default: Date.now,
-  },
-  visualInspection: {
-    isInletClear: Boolean,
-    isUndamaged: Boolean,
-    isFilterClean: {
-      wasClean: Boolean,
-      wasReplaced: Boolean,
-    },
-  },
-  functionalInspection: {
-    targetValue: Number,
-    targetGas: String,
-    measureUnit: {
-      type: String,
-      required: true,
-      enum: ['Vol%', 'ppm', '%LEL'],
-    },
-    calibrationPre: Number,
-    calibrationAft: Number,
-  },
-  hasPassedInspection: {
-    hasPassed: Boolean,
-    comment: {
-      type: String,
-      maxlength: [500, 'Comment can not be longer than 500 characters.'],
-    },
   },
 });
 
