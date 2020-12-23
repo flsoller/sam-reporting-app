ab44d5770dd21d3f881662333e99342a4ec8a0bb
Florian Soller <flsoller@gmail.com>
2020-12-23 18:04:40 +0100

    Match sample report data to database sample model.

--- backend/reports/sampleReport.js
+++ backend/reports/sampleReport.js
@@ -6,9 +6,17 @@ const generateReport = (res, data) => {
   doc.pipe(res);
 
   doc.text('Hello World!');
-  doc.text(
-    `This is data passed into the generator: ${data.name}, ${data.desc}, ${data.serialNumber} `
-  );
+  doc.text('This is data passed into the generator:');
+  doc.text(`
+    ${data.customerName}, 
+    ${data.installLocation}, 
+    ${data.street}, 
+    ${data.city}, 
+    ${data.country}, 
+    ${data.productName}, 
+    ${data.serialNumber}, 
+    ${data.maintenanceDate}.
+    `);
 
   doc.end();
 };
