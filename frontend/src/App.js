762452245ec3426016de8b5b451f3120efa6f432
Florian Soller <flsoller@gmail.com>
2020-12-25 17:31:25 +0100

    Implement react router and import header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // Component imports
+import Header from './components/Header';
 import ExampleForm from './components/forms/ExampleForm';
+import ReportSelection from './components/forms/ReportSelection';
 
 const App = () => {
-  return <ExampleForm />;
+  return (
+    <Router>
+      <Header />
+      <Route path="/" component={ExampleForm} exact />
+      <Route path="/report" component={ReportSelection} />
+    </Router>
+  );
 };
 
 export default App;
