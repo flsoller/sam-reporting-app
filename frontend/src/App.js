4282868449432514669615d5edab75a9f6fa1fcd
Florian Soller <flsoller@gmail.com>
2020-12-26 17:59:13 +0100

    Wrap screens in material-ui container.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+// M-UI imports
+import { Container } from '@material-ui/core';
+
 // Component imports
 import Header from './components/Header';
 import ExampleForm from './components/forms/ExampleForm';
-import ReportSelection from './components/forms/ReportSelection';
+import ReportSelection from './screens/ReportSelection';
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Route path="/" component={ExampleForm} exact />
-      <Route path="/report" component={ReportSelection} />
+      <Container>
+        <Route path="/" component={ExampleForm} exact />
+        <Route path="/report" component={ReportSelection} />
+      </Container>
     </Router>
   );
 };
