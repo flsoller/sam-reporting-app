e49db73c483b6f7dbc8038b945ae33a62d89326c
Florian Soller <flsoller@gmail.com>
2020-12-28 17:37:01 +0100

    Add Drawer, CssBaseline. Revise content structure.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,21 +2,41 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // M-UI imports
-import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Toolbar, CssBaseline } from '@material-ui/core';
 
 // Component imports
-import Header from './components/Header';
+import AppHeader from './components/AppHeader';
+import DrawerMenu from './components/DrawerMenu';
 import ExampleForm from './components/forms/ExampleForm';
 import ReportSelection from './screens/ReportSelection';
 
+// Style definitions for M-UI
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
 const App = () => {
+  const classes = useStyles();
+
   return (
     <Router>
-      <Header />
-      <Container>
-        <Route path="/" component={ExampleForm} exact />
-        <Route path="/report" component={ReportSelection} />
-      </Container>
+      <CssBaseline />
+      <div className={classes.root}>
+        <AppHeader />
+        <DrawerMenu />
+        <main className={classes.content}>
+          <Toolbar variant="dense" />
+          <Route path="/" component={ExampleForm} exact />
+          <Route path="/report" component={ReportSelection} />
+        </main>
+      </div>
     </Router>
   );
 };
