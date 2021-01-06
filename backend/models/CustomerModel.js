07a6e11a81d64ed3639ad56d498dabcf7472c450
Florian Soller <flsoller@gmail.com>
2021-01-06 20:45:53 +0100

    Create customer model.

---
+++ backend/models/CustomerModel.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const customerSchema = new mongoose.Schema({
+  customerName: {
+    type: String,
+    required: true,
+  },
+  customerAddress: {
+    street: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  customerId: {
+    type: String,
+  },
+  customerRef: {
+    type: String,
+  },
+});
+
+const Customer = mongoose.model('Customer', customerSchema);
+
+export default Customer;
