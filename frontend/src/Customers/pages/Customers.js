eb7df295f120d63b29ec8bca4482d6ed9bbfca78
Florian Soller <flsoller@gmail.com>
2021-01-15 17:10:36 +0100

    Create function to get customer data from backend.

--- frontend/src/Customers/pages/Customers.js
+++ frontend/src/Customers/pages/Customers.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import DataTable from '../components/DataTable';
+import axios from 'axios';
 
 const Customers = () => {
+  // State
+  const [customerData, setCustomerData] = useState([]);
+
+  // Get customer data array from server on first render
+  const getCustomerData = async () => {
+    try {
+      const { data } = await axios.get('api/v1/customers');
+      setCustomerData(data);
+      console.log(data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getCustomerData();
+  }, []);
+
   return (
     <div>
       <h1>Customer Overview</h1>
-      <DataTable />
+      <DataTable customerDataSet={customerData} />
     </div>
   );
 };
