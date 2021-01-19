c7ddedbc4b7a87c48acf4fae9c478be8241fc27c
Florian Soller <flsoller@gmail.com>
2021-01-19 14:16:27 +0100

    Add tests to cover all input values.

--- frontend/src/Customers/pages/__test__/NewCustomer.test.js
+++ frontend/src/Customers/pages/__test__/NewCustomer.test.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import NewCustomer from '../NewCustomer';
 
-describe('customer address values', () => {
+describe('values for input fields update correctly', () => {
   let queryByLabelText;
 
   beforeEach(() => {
     return ({ queryByLabelText } = render(<NewCustomer />));
+  });
+
+  it('updates customer name value on change', () => {
+    const customerNameInput = queryByLabelText('Customer Name');
+    fireEvent.change(customerNameInput, { target: { value: 'test customer' } });
+    expect(customerNameInput.value).toBe('test customer');
   });
 
   it('updates address field value on change', () => {
@@ -33,5 +39,17 @@ describe('customer address values', () => {
     const countryInput = queryByLabelText('Country');
     fireEvent.change(countryInput, { target: { value: 'test country' } });
     expect(countryInput.value).toBe('test country');
+  });
+
+  it('updates customer reference field value on change', () => {
+    const customerRefInput = queryByLabelText('Customer Reference');
+    fireEvent.change(customerRefInput, { target: { value: 'test ref' } });
+    expect(customerRefInput.value).toBe('test ref');
+  });
+
+  it('updates customer id field value on change', () => {
+    const customerIdInput = queryByLabelText('Customer ID');
+    fireEvent.change(customerIdInput, { target: { value: 'test id' } });
+    expect(customerIdInput.value).toBe('test id');
   });
 });
