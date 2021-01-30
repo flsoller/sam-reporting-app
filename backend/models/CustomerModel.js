cf68f65334baaf5a909f8fbad597fcea26429055
Florian Soller <flsoller@gmail.com>
2021-01-30 13:02:09 +0100

    Add validation error messages to required keys.

--- backend/models/CustomerModel.js
+++ backend/models/CustomerModel.js
@@ -3,24 +3,24 @@ import mongoose from 'mongoose';
 const customerSchema = new mongoose.Schema({
   customerName: {
     type: String,
-    required: true,
+    required: [true, 'Customer name is required.'],
   },
   customerAddress: {
     address: {
       type: String,
-      required: true,
+      required: [true, 'Customer address is required.'],
     },
     city: {
       type: String,
-      required: true,
+      required: [true, 'City is required.'],
     },
     postalCode: {
       type: String,
-      required: true,
+      required: [true, 'Postal code is required.'],
     },
     country: {
       type: String,
-      required: true,
+      required: [true, 'Country is required.'],
     },
   },
   customerId: {
