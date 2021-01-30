39ea46a556e6e8dd2d5d74c1582cd800805e7755
Florian Soller <flsoller@gmail.com>
2021-01-30 13:25:15 +0100

    Fix for server returning 500 on validation error.

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,7 +1,14 @@
+import mongoose from 'mongoose';
+
 const errorHandler = (err, req, res, next) => {
-  const errorCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(errorCode);
-  res.json({ message: err.message });
+  if (mongoose.Error.ValidationError) {
+    res.status(400);
+    res.json({ message: err.message });
+  } else {
+    const errorCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(errorCode);
+    res.json({ message: err.message });
+  }
 };
 
 export { errorHandler };
