63d05ce90ed5a4f1bcf51a1789b861accb1670fd
Florian Soller <flsoller@gmail.com>
2021-02-16 18:36:48 +0100

    Create controller method to get a customer by id.

--- backend/Customers/controllers/customerController.js
+++ backend/Customers/controllers/customerController.js
@@ -41,3 +41,17 @@ export const getAllCustomers = asyncHandler(async (req, res, next) => {
   const allCustomers = await Customer.find({});
   res.status(200).json(allCustomers);
 });
+
+// @desc    Return single customer from database
+// @route   GET /api/v1/customers/:id
+// @access  Private
+export const getCustomerById = asyncHandler(async (req, res, next) => {
+  const customer = await Customer.findById(req.params.id);
+
+  if (customer) {
+    res.status(200).json(customer);
+  } else {
+    res.status(404);
+    throw new Error('Customer not found');
+  }
+});
