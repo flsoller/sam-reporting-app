d2b44f18877e55714117ff455799cae4cff1d952
Florian Soller <flsoller@gmail.com>
2021-05-17 18:48:18 +0200

    Extend with primary color from material.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3F51B5",
+      },
+    },
   },
   variants: {
     extend: {},
