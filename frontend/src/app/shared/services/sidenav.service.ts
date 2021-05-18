e2aa339cf1dd3f823f9cdd5d6849fb0c41e838fb
Florian Soller <flsoller@gmail.com>
2021-05-18 20:49:23 +0200

    Create sidenav service.

---
+++ frontend/src/app/shared/services/sidenav.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SidenavService {
+  toggleRequest = new Subject<string[]>();
+
+  private menuWidth: string[] = ['w-56', 'p-4'];
+  private open: boolean = true;
+
+  initialSidenav() {
+    return this.menuWidth;
+  }
+
+  toggleMenu() {
+    this.open = !this.open;
+    this.open === false
+      ? (this.menuWidth = ['w-16', 'p-0'])
+      : (this.menuWidth = ['w-56', 'p-4']);
+    this.toggleRequest.next(this.menuWidth);
+  }
+}
