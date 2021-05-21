2acbc810ada625a522afd12555f9df2b86d449d3
Florian Soller <flsoller@gmail.com>
2021-05-21 16:45:36 +0200

    Create customer module with routing.

---
+++ frontend/src/app/customers/customers-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+
+import { CustomerListComponent } from './customer-list/customer-list.component';
+
+const customerRoutes: Routes = [
+  {
+    path: '',
+    component: CustomerListComponent,
+  },
+  { path: 'new', component: CustomerDetailsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(customerRoutes)],
+  exports: [RouterModule],
+})
+export class CustomersRoutingModule {}
--- frontend/src/app/customers/customers.module.ts
+++ frontend/src/app/customers/customers.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomersRoutingModule } from './customers-routing.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CustomerDetailsComponent, CustomerListComponent],
+  imports: [CommonModule, CustomersRoutingModule],
 })
-export class CustomersModule { }
+export class CustomersModule {}
