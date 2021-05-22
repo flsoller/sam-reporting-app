f9009458925ce58fe3f81937efb5ef9f1cc5403c
Florian Soller <flsoller@gmail.com>
2021-05-22 21:07:27 +0200

    Add customer details form.

--- frontend/src/app/customers/customer-details/customer-details.component.ts
+++ frontend/src/app/customers/customer-details/customer-details.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-customer-details',
   templateUrl: './customer-details.component.html',
-  styleUrls: ['./customer-details.component.scss']
+  styleUrls: ['./customer-details.component.scss'],
 })
 export class CustomerDetailsComponent implements OnInit {
+  customerForm = this.fb.group({
+    customerName: [''],
+    customerAddress: this.fb.group({
+      streetAddress: [''],
+      city: [''],
+      postalCode: [''],
+      country: [''],
+    }),
+    customerId: [''],
+    customerRef: [''],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    // HANDLE SUBMIT
   }
-
 }
--- frontend/src/app/customers/customers.module.ts
+++ frontend/src/app/customers/customers.module.ts
@@ -1,12 +1,27 @@
+// Module imports
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
+// Material imports
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+
+// Component imports
 import { CustomerDetailsComponent } from './customer-details/customer-details.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { CustomersRoutingModule } from './customers-routing.module';
 
 @NgModule({
   declarations: [CustomerDetailsComponent, CustomerListComponent],
-  imports: [CommonModule, CustomersRoutingModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    CustomersRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+  ],
 })
 export class CustomersModule {}
