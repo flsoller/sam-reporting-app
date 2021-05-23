f283dfad5d685b79519d5e4fa6c959d6a458cbf5
Florian Soller <flsoller@gmail.com>
2021-05-23 17:42:09 +0200

    Add schema and form import.

--- frontend/src/app/customers/customer-details/customer-details.component.spec.ts
+++ frontend/src/app/customers/customer-details/customer-details.component.spec.ts
@@ -1,4 +1,6 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CustomerDetailsComponent } from './customer-details.component';
 
@@ -8,9 +10,10 @@ describe('CustomerDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CustomerDetailsComponent ]
-    })
-    .compileComponents();
+      declarations: [CustomerDetailsComponent],
+      imports: [ReactiveFormsModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
