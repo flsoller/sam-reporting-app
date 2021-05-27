607974eb483b7efa388e56e8072a6599ec28406b
Florian Soller <flsoller@gmail.com>
2021-05-27 18:55:33 +0200

    Extend API service with error handler.

--- frontend/src/app/customers/customer-api.service.ts
+++ frontend/src/app/customers/customer-api.service.ts
@@ -1,7 +1,18 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+// Service imports
+import { SnackBarService } from '../shared/services/snackbar.service';
 import { CustomerService } from './customer.service';
+
+// Models
 import { Customer } from './models/customer.model';
+
+export interface NewCustomerResponse {
+  message: string;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +22,8 @@ export class CustomerApiService {
 
   constructor(
     private http: HttpClient,
-    private customerService: CustomerService
+    private customerService: CustomerService,
+    private snackBar: SnackBarService
   ) {}
 
   getCustomers() {
@@ -21,9 +33,29 @@ export class CustomerApiService {
   }
 
   newCustomer(data: Customer) {
-    this.http.post(this.baseURL, data).subscribe((res) => {
-      // Implement messenger service?
-      console.log(res);
-    });
+    return this.http
+      .post<NewCustomerResponse>(this.baseURL, data)
+      .pipe(catchError(this.handleError.bind(this)));
+  }
+
+  //
+  // ERROR HANDLING
+  //
+
+  private handleError(errorResponse: HttpErrorResponse) {
+    const validationError = 'Customer validation failed';
+    const existsError = 'Customer already exsists';
+    let errMsg = 'Something went wrong';
+
+    if (errorResponse.error.message.split(':')[0] === validationError) {
+      errMsg = 'Please fill out all required fields.';
+    }
+
+    if (errorResponse.error.message === existsError) {
+      errMsg = `${existsError}.`;
+    }
+
+    this.snackBar.showSnackBar(errMsg, 'error-snackbar');
+    return throwError(errMsg);
   }
 }
