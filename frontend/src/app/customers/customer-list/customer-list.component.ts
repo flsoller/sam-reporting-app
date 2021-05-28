f61ccdcde80ac8a57dc37892ddd9bceb3bfe90bd
Florian Soller <flsoller@gmail.com>
2021-05-28 21:48:13 +0200

    Set up data and subscription.

--- frontend/src/app/customers/customer-list/customer-list.component.ts
+++ frontend/src/app/customers/customer-list/customer-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CustomerService } from '../customer.service';
+import { Customer } from '../models/customer.model';
 
 @Component({
   selector: 'app-customer-list',
   templateUrl: './customer-list.component.html',
-  styleUrls: ['./customer-list.component.scss']
+  styleUrls: ['./customer-list.component.scss'],
 })
-export class CustomerListComponent implements OnInit {
+export class CustomerListComponent implements OnInit, OnDestroy {
+  customers: Customer[] = [];
+  subscription: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private customerService: CustomerService) {}
 
   ngOnInit(): void {
+    this.subscription = this.customerService.customersChanged.subscribe(
+      (customers: Customer[]) => {
+        this.customers = customers;
+      }
+    );
+
+    this.customers = this.customerService.getCustomers();
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
