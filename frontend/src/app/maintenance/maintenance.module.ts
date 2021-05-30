f045e52333a34984c16af28842fb7c1e65aff907
Florian Soller <flsoller@gmail.com>
2021-05-30 20:39:47 +0200

    Add router.

--- frontend/src/app/maintenance/maintenance.module.ts
+++ frontend/src/app/maintenance/maintenance.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Component imports
+import { MaintenanceStartComponent } from './maintenance-start/maintenance-start.component';
 
+// Module imports
+import { MaintenanceRouterModule } from './maintenance-routing.module';
+
+// Material imports
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [],
+  declarations: [MaintenanceStartComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MaintenanceRouterModule,
+  ],
 })
-export class MaintenanceModule { }
+export class MaintenanceModule {}
