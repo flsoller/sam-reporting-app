f659dfcbcfc93a534777d4378e538668da45cc8d
Florian Soller <flsoller@gmail.com>
2021-06-01 15:16:19 +0200

    Add route to portable instruments.

--- frontend/src/app/maintenance/maintenance-routing.module.ts
+++ frontend/src/app/maintenance/maintenance-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MaintenanceStartComponent } from './maintenance-start/maintenance-start.component';
+import { PortableInstrumentComponent } from './forms/portable-instrument/portable-instrument.component';
 
 const maintenanceRoutes: Routes = [
   {
     path: '',
     component: MaintenanceStartComponent,
     children: [],
+  },
+  {
+    path: 'portable',
+    component: PortableInstrumentComponent,
   },
 ];
 
