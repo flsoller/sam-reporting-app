291dcc8daea52bf8b9f6f2d5714c45a5c58320a3
Florian Soller <flsoller@gmail.com>
2021-06-01 15:16:28 +0200

    Add component and material imports.

--- frontend/src/app/maintenance/maintenance.module.ts
+++ frontend/src/app/maintenance/maintenance.module.ts
@@ -1,23 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // Component imports
 import { MaintenanceStartComponent } from './maintenance-start/maintenance-start.component';
+import { PortableInstrumentComponent } from './forms/portable-instrument/portable-instrument.component';
+import { SensorDataComponent } from './components/sensor-data/sensor-data.component';
 
-// Module imports
+// App Module imports
 import { MaintenanceRouterModule } from './maintenance-routing.module';
 
 // Material imports
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [MaintenanceStartComponent],
+  declarations: [
+    MaintenanceStartComponent,
+    PortableInstrumentComponent,
+    SensorDataComponent,
+  ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     MatSelectModule,
     MatFormFieldModule,
     MaintenanceRouterModule,
+    MatInputModule,
+    MatButtonModule,
   ],
 })
 export class MaintenanceModule {}
