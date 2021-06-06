9317de1688ea9391040f35a9ed2150ce12da355e
Florian Soller <flsoller@gmail.com>
2021-06-06 17:13:01 +0200

    Add initial instrument form.

--- frontend/src/app/maintenance/forms/portable-instrument/portable-instrument.component.ts
+++ frontend/src/app/maintenance/forms/portable-instrument/portable-instrument.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-portable-instrument',
   templateUrl: './portable-instrument.component.html',
-  styleUrls: ['./portable-instrument.component.scss']
+  styleUrls: ['./portable-instrument.component.scss'],
 })
 export class PortableInstrumentComponent implements OnInit {
+  instrumentData = this.fb.group({
+    instrumentName: [''],
+    instrumentSerialNumber: [''],
+    testDate: [''],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onSubmit() {}
 }
