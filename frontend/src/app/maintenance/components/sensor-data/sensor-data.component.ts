e4e8e0503856de8d19a2937065111c7f948603ec
Florian Soller <flsoller@gmail.com>
2021-06-08 16:35:46 +0200

    Add input to receive form data from parent.

--- frontend/src/app/maintenance/components/sensor-data/sensor-data.component.ts
+++ frontend/src/app/maintenance/components/sensor-data/sensor-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 
 import { GasUnit } from '../../models/sensor.model';
@@ -9,39 +9,16 @@ import { GasUnit } from '../../models/sensor.model';
   styleUrls: ['./sensor-data.component.scss'],
 })
 export class SensorDataComponent implements OnInit {
+  @Input() form: any;
+
   refGas: boolean = false;
   calPanelState: boolean = true;
 
   gasUnit: GasUnit[] = [GasUnit.LEL, GasUnit.PPM, GasUnit.VOL];
 
-  sensorData = this.fb.group({
-    serialNumber: [''],
-    calGasName: [''],
-    calGasConc: [],
-    calGasUnit: [''],
-    refGas: this.fb.group({
-      isUsed: [false],
-      refGasName: [''],
-      refGasConc: [],
-      refGasUnit: [''],
-    }),
-    alarmLvls: this.fb.group({
-      alarmOne: [],
-      alarmTwo: [],
-      // alarmThree: [], Optional display for fixed instruments
-      // alarmFour: [], Optional display for fixed instruments
-      stel: [],
-      twa: [],
-    }),
-  });
-
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
-
-  onSubmit() {
-    console.log(this.sensorData.value);
-  }
 
   onToggleRefGas() {
     this.refGas = !this.refGas;
