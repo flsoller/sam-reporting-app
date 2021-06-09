6b50ec9d4c036a6743b6e8e190650ff3615c30b2
Florian Soller <flsoller@gmail.com>
2021-06-09 18:02:00 +0200

    Add update method for instruments data.

--- frontend/src/app/maintenance/portable-maintenance.service.ts
+++ frontend/src/app/maintenance/portable-maintenance.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { v4 as uuidv4 } from 'uuid';
 
 import { PortableMaintenance } from './models/portable-maintenance.model';
+import { PortableUnit } from './models/portable.model';
 
 @Injectable({
   providedIn: 'root',
@@ -19,5 +20,13 @@ export class PortableMaintenanceService {
     // Logging
     console.log(`Maintenance created with data:`);
     console.log(this.maintenanceData[0]);
+  }
+
+  updateData(id: string, instrumentsData: PortableUnit[]) {
+    this.maintenanceData.map((maintenance) => {
+      if (maintenance.jobID === id) {
+        maintenance.instruments = instrumentsData;
+      }
+    });
   }
 }
