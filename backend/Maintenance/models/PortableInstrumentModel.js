eaf5855e95d032f6442d46c0bf7d45ca102181ef
Florian Soller <flsoller@gmail.com>
2021-06-16 17:43:32 +0200

    Disable id creation for child schemas.

--- backend/Maintenance/models/PortableInstrumentModel.js
+++ backend/Maintenance/models/PortableInstrumentModel.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 import SensorModel from './SensorModel.js';
 
-const PortableInstrumentModelSchema = new mongoose.Schema({
-  instrumentName: String,
-  instrumentSerialNumber: String,
-  testDate: Date,
-  sensors: [SensorModel],
-});
+const PortableInstrumentModelSchema = new mongoose.Schema(
+  {
+    instrumentName: String,
+    instrumentSerialNumber: String,
+    testDate: Date,
+    sensors: [SensorModel],
+  },
+  { _id: false }
+);
 
 export default PortableInstrumentModelSchema;
--- backend/Maintenance/models/SensorModel.js
+++ backend/Maintenance/models/SensorModel.js
@@ -1,29 +1,32 @@
 import mongoose from 'mongoose';
 
-const SensorModelSchema = new mongoose.Schema({
-  serialNumber: String,
-  calGasName: String,
-  calGasConc: Number,
-  calGasUnit: {
-    type: String,
-    enum: ['%LEL', 'PPM', '%Vol'],
-  },
-  refGas: {
-    isUsed: Boolean,
-    refGasName: String,
-    refGasConc: Number,
-    refGasUnit: String,
-  },
-  alarmLvls: {
-    alarmOne: Number,
-    alarmTwo: Number,
-    alarmThree: Number,
-    alarmFour: Number,
-    additional: {
-      stel: Number,
-      twa: Number,
+const SensorModelSchema = new mongoose.Schema(
+  {
+    serialNumber: String,
+    calGasName: String,
+    calGasConc: Number,
+    calGasUnit: {
+      type: String,
+      enum: ['%LEL', 'PPM', '%Vol'],
+    },
+    refGas: {
+      isUsed: Boolean,
+      refGasName: String,
+      refGasConc: Number,
+      refGasUnit: String,
+    },
+    alarmLvls: {
+      alarmOne: Number,
+      alarmTwo: Number,
+      alarmThree: Number,
+      alarmFour: Number,
+      additional: {
+        stel: Number,
+        twa: Number,
+      },
     },
   },
-});
+  { _id: false }
+);
 
 export default SensorModelSchema;
