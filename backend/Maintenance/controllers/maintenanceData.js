17b42292b347f0a2df9750ed1c947292d80e5abf
Florian Soller <flsoller@gmail.com>
2021-06-17 19:44:23 +0200

    Add controller method to get data by customer.

--- backend/Maintenance/controllers/maintenanceData.js
+++ backend/Maintenance/controllers/maintenanceData.js
@@ -2,19 +2,6 @@ import asyncHandler from '../../middleware/asyncHandler.js';
 
 import PortableMaintenanceModel from '../models/PortableMaintenanceModel.js';
 
-// @desc    Get all maintenance db entries
-// @route   GET /api/v1/maintenance-data
-// @access  Private
-// export const getMaintenanceData = async (req, res, next) => {
-//   const allMaintenanceData = await SampleModel.find({});
-
-//   try {
-//     res.status(200).json(allMaintenanceData);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
 // @desc    Create new entry for maintenance data document (portableInstruments)
 // @route   POST /api/v1/maintenance-data/portable
 // @access  Private
@@ -29,6 +16,26 @@ export const addPortableMaintenanceData = asyncHandler(
       });
     } else {
       throw new Error('Invalid maintenance data');
+    }
+  }
+);
+
+// @desc    Get all portable maintenance entries for specific customer
+// @route   GET /api/v1/maintenance-data/portable/
+// @access  Private
+export const getPortableMaintDataByCustomer = asyncHandler(
+  async (req, res, next) => {
+    const { customerName } = req.body;
+
+    const portableMaintenance = await PortableMaintenanceModel.findOne({
+      customerName,
+    });
+
+    if (portableMaintenance) {
+      res.status(200).json(portableMaintenance);
+    } else {
+      res.status(400);
+      throw new Error('No data available.');
     }
   }
 );
