f71e8ed4d8c08a0e3aff1515e27477f3d58a67c5
Florian Soller <flsoller@gmail.com>
2021-06-18 11:11:41 +0200

    Remove sample report.
    To be replaced with correct models later. Needed to fix compile error.

--- backend/Reports/controllers/generateReport.js
+++ backend/Reports/controllers/generateReport.js
@@ -1,13 +1,12 @@
 import { generateReport } from '../templates/sampleReport.js';
-import SampleModel from '../../Maintenance/models/SampleModel.js';
 
 // @desc    Generate and return PDF report.
 // @route   GET /api/v1/generate-pdf/:id
 // @access  Private
-const getReport = async (req, res, next) => {
-  const data = await SampleModel.findById(req.params.id);
+// const getReport = async (req, res, next) => {
+//   const data = await SampleModel.findById(req.params.id);
 
-  generateReport(res, data);
-};
+//   generateReport(res, data);
+// };
 
-export { getReport };
+// export { getReport };
--- backend/Reports/routes/generateReport.js
+++ backend/Reports/routes/generateReport.js
@@ -2,12 +2,12 @@
 import express from 'express';
 
 // Controller imports
-import { getReport } from '../controllers/generateReport.js';
+// import { getReport } from '../controllers/generateReport.js';
 
 // Initialize router
 const router = express.Router();
 
 // Route definitions
-router.route('/:id').get(getReport);
+// router.route('/:id').get(getReport);
 
 export default router;
