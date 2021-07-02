b3326cbd631389fd08fa8ab78952f3d17065edb3
Florian Soller <flsoller@gmail.com>
2021-07-02 16:28:10 +0200

    Add general data section to report.

--- backend/Reports/templates/portableInstrument.js
+++ backend/Reports/templates/portableInstrument.js
@@ -1,6 +1,11 @@
 import PDFDocument from 'pdfkit';
 
-import { createHeader, createFooter } from './reportBuilder.js';
+import {
+  createHeader,
+  createFooter,
+  createDataBlock,
+  createSectionHeader,
+} from './reportBuilder.js';
 
 export const generateReport = (res, data) => {
   const doc = new PDFDocument({ size: 'A4', margin: 25 });
@@ -8,10 +13,39 @@ export const generateReport = (res, data) => {
   doc.pipe(res);
 
   data[0].instruments.forEach((instrument, index) => {
+    // Page header
     createHeader(doc, instrument.instrumentName);
 
+    //
+    // General data section
+    //
+    createSectionHeader(doc, 'General Data', 25, 100);
+
+    createDataBlock(
+      doc,
+      'Serial Number',
+      instrument.instrumentSerialNumber,
+      25,
+      130
+    );
+
+    createDataBlock(
+      doc,
+      'Test Date',
+      instrument.testDate.toISOString().split('T')[0],
+      220,
+      130
+    );
+
+    //
+    // Sensor data section
+    //
+    createSectionHeader(doc, 'Sensor Data', 25, 250);
+
+    // Page footer
     createFooter(doc);
 
+    // Create new page for next instrument in array
     if (index !== data[0].instruments.length - 1) {
       doc.addPage();
     }
