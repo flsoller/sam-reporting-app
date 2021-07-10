7d4a0ad43bc39af43e36bf8b779961fb3cd062bd
Florian Soller <flsoller@gmail.com>
2021-07-10 10:55:27 +0200

    Add edit mode for component.

--- frontend/src/app/customers/customer-details/customer-details.component.ts
+++ frontend/src/app/customers/customer-details/customer-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { SnackBarService } from 'src/app/shared/services/snackbar.service';
 import { CustomerApiService } from '../customer-api.service';
 
@@ -10,8 +12,13 @@ import { CustomerApiService } from '../customer-api.service';
 })
 export class CustomerDetailsComponent implements OnInit {
   submitDisabled = false;
-  isEdit = false;
 
+  // Properties for editing customers
+  paramSub: Subscription = new Subscription();
+  isEdit = false;
+  customerId!: string | null;
+
+  // Form builder
   customerForm = this.fb.group({
     customerName: [''],
     customerAddress: this.fb.group({
@@ -27,16 +34,24 @@ export class CustomerDetailsComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private customerApi: CustomerApiService,
-    private snackBarService: SnackBarService
+    private snackBarService: SnackBarService,
+    private route: ActivatedRoute
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.paramSub = this.route.paramMap.subscribe((param) => {
+      this.customerId = param.get('id');
+      this.isEdit = param.get('edit') === 'edit' ? true : false;
+    });
+  }
 
   onSubmit() {
-    this.customerApi.newCustomer(this.customerForm.value).subscribe((res) => {
-      this.snackBarService.showSnackBar(res.message);
-      this.customerForm.reset();
-      this.customerApi.getCustomers();
-    });
+    if (!this.isEdit) {
+      this.customerApi.newCustomer(this.customerForm.value).subscribe((res) => {
+        this.snackBarService.showSnackBar(res.message);
+        this.customerForm.reset();
+        this.customerApi.getCustomers();
+      });
+    }
   }
 }
