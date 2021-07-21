d4b22e469f6bb7e87eab4f46f703b10cfdbf549e
Florian Soller <flsoller@gmail.com>
2021-07-21 21:17:07 +0200

    Add password hashing pre-save if value changed.

--- backend/Auth/models/userModel.js
+++ backend/Auth/models/userModel.js
@@ -26,6 +26,15 @@ userSchema.methods.checkPassword = async function (incomingPw) {
   return await bcrypt.compare(incomingPw, this.password);
 };
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
