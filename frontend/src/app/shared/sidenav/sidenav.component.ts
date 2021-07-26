6987a125aee2be1773fde831c7a69fc0571e404f
Florian Soller <flsoller@gmail.com>
2021-07-26 17:31:25 +0200

    Hide sidenav if login screen is active.

--- frontend/src/app/shared/sidenav/sidenav.component.ts
+++ frontend/src/app/shared/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { SidenavService } from '../services/sidenav.service';
 
 @Component({
@@ -10,7 +11,7 @@ export class SidenavComponent implements OnInit {
   menuWidth: string[] = [];
   open = true;
 
-  constructor(private sidenavService: SidenavService) {}
+  constructor(private sidenavService: SidenavService, public router: Router) {}
 
   ngOnInit(): void {
     this.menuWidth = this.sidenavService.initialSidenav();
