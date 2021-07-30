d8c3a127d49d6e6757bc5f11c1cc4bf130960df2
Florian Soller <flsoller@gmail.com>
2021-07-30 13:57:02 +0200

    Add route protection for reports.

--- backend/Reports/routes/generateReport.js
+++ backend/Reports/routes/generateReport.js
@@ -1,6 +1,9 @@
 // Package imports
 import express from 'express';
 
+// Middleware
+import { protectedRoute } from '../../middleware/authMiddleware.js';
+
 // Controller imports
 import { getPortableReport } from '../controllers/generateReport.js';
 
@@ -8,6 +11,6 @@ import { getPortableReport } from '../controllers/generateReport.js';
 const router = express.Router();
 
 // Route definitions
-router.route('/portable/:jobId').get(getPortableReport);
+router.route('/portable/:jobId').get(protectedRoute, getPortableReport);
 
 export default router;
