f20cf259b3060424de6d571ce730273c4161c450
Florian Soller <flsoller@gmail.com>
2021-07-31 14:50:12 +0200

    Extend model with validation and User ref.

--- backend/Maintenance/models/PortableMaintenanceModel.js
+++ backend/Maintenance/models/PortableMaintenanceModel.js
@@ -2,8 +2,20 @@ import mongoose from 'mongoose';
 import PortableInstrumentModel from './PortableInstrumentModel.js';
 
 const PortableMaintenanceModelSchema = new mongoose.Schema({
-  jobID: String,
-  customer: String,
+  jobID: {
+    type: String,
+    required: [true, 'Job ID is required'],
+    unique: true,
+  },
+  technician: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'A user needs to be associated with the maintenance.'],
+  },
+  customer: {
+    type: String,
+    required: [true, 'Customer is required.'],
+  },
   instruments: [PortableInstrumentModel],
 });
 
