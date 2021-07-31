91e5bc7a02a99f49d93736b3ba68eac35814aa65
Florian Soller <flsoller@gmail.com>
2021-07-31 14:48:45 +0200

    Set custom err msg for unique fields err.

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 const errorHandler = (err, req, res, next) => {
   if (mongoose.Error.ValidationError) {
     res.status(400);
-    res.json({ message: err.message });
+    err.code === 11000
+      ? res.json({ message: 'A resource with that ID already exists.' })
+      : res.json({ message: err.message });
   } else {
     const errorCode = res.statusCode === 200 ? 500 : res.statusCode;
     res.status(errorCode);
