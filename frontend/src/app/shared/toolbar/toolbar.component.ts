e270c634fdab0ede541f0a14c087b74d87e08478
Florian Soller <flsoller@gmail.com>
2021-08-04 17:18:28 +0200

    Add profile link button with name initials.

--- frontend/src/app/shared/toolbar/toolbar.component.ts
+++ frontend/src/app/shared/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 import { SidenavService } from '../services/sidenav.service';
 
 @Component({
@@ -7,9 +8,17 @@ import { SidenavService } from '../services/sidenav.service';
   styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
-  constructor(private sidenavService: SidenavService) {}
+  nameInitial = '';
 
-  ngOnInit(): void {}
+  constructor(
+    private sidenavService: SidenavService,
+    private auth: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.nameInitial =
+      this.auth.user.value?.name.split('')[0].toLocaleUpperCase() || '';
+  }
 
   onToggleRequest() {
     this.sidenavService.toggleMenu();
