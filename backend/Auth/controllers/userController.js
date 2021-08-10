61c0a84bd854e799ee2196c536f8894458c8c252
Florian Soller <flsoller@gmail.com>
2021-08-10 17:39:02 +0200

    Add route + controller for updating user profile.

--- backend/Auth/controllers/userController.js
+++ backend/Auth/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from '../../middleware/asyncHandler.js';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 import { createToken } from '../../utils/jwtHelper.js';
@@ -42,6 +43,43 @@ export const getUserProfile = asyncHandler(async (req, res, next) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+// @desc    Update user profile
+// @route   POST /api/v1/users/profile
+// @access  Private
+export const updateUser = asyncHandler(async (req, res, next) => {
+  const { email, password, name, newPassword } = req.body;
+
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = name || user.name;
+    user.email = email || user.email;
+
+    if (newPassword && (await user.checkPassword(password))) {
+      user.password = newPassword;
+    } else if (!(await user.checkPassword(password))) {
+      res.status(404);
+      throw new Error('User not updated');
+    }
+
+    const updatedUser = await user.save();
+    const token = createToken(updatedUser._id);
+    const { exp } = jwt.verify(token, process.env.JWT_KEY);
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token,
+      expiration: exp,
     });
   } else {
     res.status(404);
--- backend/Auth/routes/userRoutes.js
+++ backend/Auth/routes/userRoutes.js
@@ -2,7 +2,11 @@
 import express from 'express';
 
 // Controller imports
-import { authUser, getUserProfile } from '../controllers/userController.js';
+import {
+  authUser,
+  getUserProfile,
+  updateUser,
+} from '../controllers/userController.js';
 
 // Middleware
 import { protectedRoute } from '../../middleware/authMiddleware.js';
@@ -12,6 +16,9 @@ const router = express.Router();
 
 // Route definitions
 router.route('/login').post(authUser);
-router.route('/profile').get(protectedRoute, getUserProfile);
+router
+  .route('/profile')
+  .get(protectedRoute, getUserProfile)
+  .post(protectedRoute, updateUser);
 
 export default router;
