80fa6b38674e50f6f33f6fd43c00aa25fa453c2a
Florian Soller <flsoller@gmail.com>
2021-08-11 17:42:13 +0200

    Add methods for updating user profile.

--- frontend/src/app/auth/user.service.ts
+++ frontend/src/app/auth/user.service.ts
@@ -1,16 +1,45 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
 
-import { UserProfile } from './user.model';
+import { UserProfile, User } from './user.model';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
   private baseUrl = '/api/v1/users';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private auth: AuthService) {}
 
   // Gets user profile from server
   getProfile() {
     return this.http.get<UserProfile>(`${this.baseUrl}/profile`);
+  }
+
+  // Update user profile without new password
+  updateUser(email: string, name: string) {
+    this.http
+      .post<User>(`${this.baseUrl}/profile`, { email, name })
+      .subscribe((user) => {
+        this.auth.user.next(user);
+      });
+  }
+
+  // Update profile, including password
+  updateUserAndPassword(
+    email: string,
+    password: string,
+    name: string,
+    newPassword: string
+  ) {
+    this.http
+      .post<User>(`${this.baseUrl}/profile`, {
+        email,
+        password,
+        name,
+        newPassword,
+      })
+      .subscribe((user) => {
+        this.auth.user.next(user);
+      });
   }
 }
