06a6626c3d52b4103ef80b581762ab41aeb173c1
Florian Soller <flsoller@gmail.com>
2021-08-17 18:59:10 +0200

    Create service for setting form fields.
    To be used with new dynamic forms module.

---
+++ frontend/src/app/maintenance/components/sensor-data/sensor-data.service.ts
@@ -0,0 +1,99 @@
+import { Injectable } from '@angular/core';
+
+import { InputBase } from 'src/app/forms/models/input-base';
+import { InputDropdown } from 'src/app/forms/models/input-dropdown';
+import { InputTextbox } from 'src/app/forms/models/input-textbox';
+import { GasUnit } from '../../models/sensor.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SensorDataService {
+  gasUnit: GasUnit[] = [GasUnit.LEL, GasUnit.PPM, GasUnit.VOL];
+
+  getCalInfoForm() {
+    const calInfo: InputBase<string>[] = [
+      new InputTextbox({
+        key: 'calGasName',
+        label: 'Calibration Gas',
+        type: 'text',
+        widthClass: 'md: w-64',
+        required: true,
+      }),
+      new InputTextbox({
+        key: 'calGasConc',
+        label: 'Concentration',
+        type: 'number',
+        widthClass: 'md: w-32',
+        required: true,
+      }),
+      new InputDropdown({
+        key: 'calGasUnit',
+        label: 'Unit',
+        widthClass: 'md: w-24',
+        required: true,
+        options: this.gasUnit.map((u) => {
+          return { key: u, value: u };
+        }),
+      }),
+    ];
+
+    return calInfo;
+  }
+
+  getCalDataForm() {
+    const calData: InputBase<string>[] = [
+      new InputTextbox({
+        key: 'preZero',
+        label: 'Pre Zero Data',
+        type: 'number',
+        widthClass: 'md: w-36',
+        required: true,
+      }),
+      new InputTextbox({
+        key: 'aftZero',
+        label: 'Aft Zero Data',
+        type: 'number',
+        widthClass: 'md: w-36',
+        required: true,
+      }),
+      new InputTextbox({
+        key: 'preCal',
+        label: 'Pre Cal Data',
+        type: 'number',
+        widthClass: 'md: w-36',
+        required: true,
+      }),
+      new InputTextbox({
+        key: 'aftCal',
+        label: 'Aft Cal Data',
+        type: 'number',
+        widthClass: 'md: w-36',
+        required: true,
+      }),
+    ];
+
+    return calData;
+  }
+
+  getRefgasInfoForm() {
+    const refInfo: InputBase<string>[] = [
+      new InputTextbox({
+        key: 'refGasName',
+        label: 'Reference Gas',
+        type: 'text',
+        widthClass: 'md: w-64',
+        required: true,
+      }),
+      new InputTextbox({
+        key: 'refGasConc',
+        label: 'Concentration',
+        type: 'number',
+        widthClass: 'md: w-32',
+        required: true,
+      }),
+    ];
+
+    return refInfo;
+  }
+}
