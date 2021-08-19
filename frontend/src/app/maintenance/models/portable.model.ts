73587df0e0c6f77d805978cf311a6f67b321138c
Florian Soller <flsoller@gmail.com>
2021-08-19 23:26:00 +0200

    Add enums for instrument names.

--- frontend/src/app/maintenance/models/portable.model.ts
+++ frontend/src/app/maintenance/models/portable.model.ts
@@ -1,5 +1,11 @@
 import { Sensor } from './sensor.model';
 
+export enum InstrumentName {
+  ALTAIR_BASE = 'Altair (Pro)',
+  ALTAIR_4X = 'Altair 4X',
+  ALTAIR_5X = 'Altair 5X',
+}
+
 export interface PortableUnit {
   instrumentName: string;
   customer: string;
