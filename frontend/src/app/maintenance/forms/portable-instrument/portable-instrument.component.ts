f01210abb91726db953cb954db41a617cbed7cc5
Florian Soller <flsoller@gmail.com>
2021-08-30 16:34:58 +0200

    Add input prop for preloading instrument data. #7.

--- frontend/src/app/maintenance/forms/portable-instrument/portable-instrument.component.ts
+++ frontend/src/app/maintenance/forms/portable-instrument/portable-instrument.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { PreLoadDeviceInfo } from '../../components/device-info/device-info.component';
+import { PortableUnit } from '../../models/portable.model';
 
 @Component({
   selector: 'app-portable-instrument',
@@ -7,12 +9,14 @@ import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./portable-instrument.component.scss'],
 })
 export class PortableInstrumentComponent implements OnInit {
-  instrumentForm: FormGroup;
-  sensors: {}[] = [{}];
-
   @Input() index!: number;
+  @Input() preLoadInstrument: PortableUnit | null = null;
   @Output() formReady: EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
   @Output() deleteRequest = new EventEmitter<number>();
+
+  instrumentForm: FormGroup;
+  sensors: {}[] = [{}];
+  deviceInfoData: PreLoadDeviceInfo | null = null;
 
   constructor(private fb: FormBuilder) {
     this.instrumentForm = this.fb.group({
@@ -22,6 +26,15 @@ export class PortableInstrumentComponent implements OnInit {
 
   ngOnInit(): void {
     this.formReady.emit(this.instrumentForm);
+
+    if (this.preLoadInstrument) {
+      this.deviceInfoData = {
+        instrumentName: this.preLoadInstrument.instrumentName,
+        instrumentSerialNumber: this.preLoadInstrument.serialNumber,
+        testDate: this.preLoadInstrument.testDate,
+      };
+      this.sensorData.patchValue([...this.preLoadInstrument.sensorData]);
+    }
   }
 
   addFormControl(controlName: string, formGroup: FormGroup) {
